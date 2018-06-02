3d5be0048657c0fc581fb0200f65efa15448a825
Scot <scot.pfleghaar@gmail.com>
2018-06-02 13:18:55 -0500

    Fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <h3>Quotes Added</h3>
             <div class="progress">
                 <div class="progress">
-                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                    <div v-if="quoteCount > 0" class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
                         :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
                         {{ quoteCount }} / {{ maxQuotes}}
                     </div>
